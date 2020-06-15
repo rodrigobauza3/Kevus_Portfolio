e5a63cbc933f87f80b77e67a042c7eea7291c875
Bauzá <r.bauza@accenture.com>
2020-06-15 01:47:31 -0300

    v2.0 (added hoverIntent and fixed videos size to avoid DOM lags + other minor bugfixes)

--- js/personalized.js
+++ js/personalized.js
@@ -1,9 +1,10 @@
 function mouseOverAction(e){
+  
   if (e.children()[0].className == "yt-video"){
-    e.html('<iframe src="' + e.attr("href") + '?rel=0&autoplay=1&mute=1&modestbranding=1&autohide=1&loop=1&playlist=' + e.attr("href").split("/").pop().split(".")[0] + '" frameborder="0" allow="autoplay"></iframe>');
+    e.html('<iframe height="'+ (e.children()[0].height - 0.41) +'" src="' + e.attr("href") + '?rel=0&autoplay=1&mute=1&modestbranding=1&autohide=1&loop=1&playlist=' + e.attr("href").split("/").pop().split(".")[0] + '" frameborder="0" allow="autoplay"></iframe>');
   }
   else if (e.children()[0].className == "lc-video"){
-    e.html('<video autoplay loop muted> <source src="' + e.attr("href") + '"></video>');
+    e.html('<video autoplay loop muted width="'+ e.children()[0].width +'" height="'+ e.children()[0].height +'"> <source src="' + e.attr("href") + '"></video>');
   }
 };
 
@@ -14,18 +15,15 @@ function mouseOutAction(e){
     e.html('<img src="https://img.youtube.com/vi/' + id + '/0.jpg" class="yt-video"><img src="img/play.png" class="video-play">');
   }
   else if (e.children()[0].tagName.toLowerCase() == "video"){
+    e.children()[0].pause();
     img = e.children()[0].innerHTML.split("/").pop().split(".")[0];
-    e.html('<img src="videos/gallery/_thumbnail/' + img + '.png" class="lc-video"><img src="img/play.png" class="video-play">');
-  }  
+    e.html('<img width="'+ e.children()[0].width +'" height="'+ e.children()[0].height +'" src="videos/gallery/_thumbnail/' + img + '.png" class="lc-video"><img src="img/play.png" class="video-play">');
+  }
 };
 
 const playOnHover = () => {
-  
-  $('.lightbox_trigger').mouseover(function(){
-      mouseOverAction($(this));
-  });    
-  $('.lightbox_trigger').mouseout(function(){
-      mouseOutAction($(this));
+  $.getScript('js/jquery.hoverIntent.js', function(){
+    $('.lightbox_trigger').hoverIntent(function(){mouseOverAction($(this))}, function(){mouseOutAction($(this))});    
   });
 }
 
@@ -78,7 +76,7 @@ function vidLocalBuilder(src, title){
           '<div id="lightbox">' +
             '<div id="lightbox-content">' +
               '<div id="lightbox-v-overlay">' +
-                  '<video controls autoplay loop> <source src="' + src + '"></video>' +
+                  '<video controls autoplay loop disablePictureInPicture oncontextmenu="return false;" controlsList="nodownload"> <source src="' + src + '"></video>' +
                   '<p>' + title + '</p>' +
               '</div>' +
             '</div>' +
@@ -101,7 +99,7 @@ $('.lightbox_trigger').click(function(e) {
       $('#lightbox-content').html('<iframe src="https://www.youtube.com/embed/' + media_name + '?rel=0&autoplay=1&modestbranding=1&autohide=1&loop=1&playlist=' + media_name + '" frameborder="0" allow="autoplay" allowfullscreen></iframe>');
     }
     else if(item[0].tagName.toLowerCase() == "video") {
-        $('#lightbox-content').html('<div id="lightbox-v-overlay"> <video controls autoplay loop> <source src="' + media_href + '"></video> <p>' + media_name + '</p></div>');
+        $('#lightbox-content').html('<div id="lightbox-v-overlay"> <video controls autoplay loop disablePictureInPicture oncontextmenu="return false;" controlsList="nodownload"> <source src="' + media_href + '"></video> <p>' + media_name + '</p></div>');
       }
     else {
       $('#lightbox-content').html('<div id="lightbox-overlay"> <img src="' + media_href + '"> <p>' + media_name + '</p></div>');
@@ -119,18 +117,21 @@ $('.lightbox_trigger').click(function(e) {
       lightbox = vidBuilder(media_name);
     }
     else if (item[0].tagName.toLowerCase() == "video"){
-      lightbox = vidLocalBuilder(media_href);
+      lightbox = vidLocalBuilder(media_href, media_name);
     }
     else {
       lightbox = imgBuilder(media_href, media_name);
     }
 
     $('body').append(lightbox);
+    disableRightClick();
+    disableScrolling();
+    // $('#lightbox').show('fast');
   }
 
   $('body').on('click', "#lightbox", function() {
     if(item[0].tagName.toLowerCase() == "iframe") {
-      $('#lightbox-content').html('<iframe src="https://www.youtube.com/embed/' + media_name + '?rel=0&modestbranding=1&autohide=1&controls=0" frameborder="0" allow="autoplay" allowfullscreen></iframe>');
+      $('#lightbox-content').html('<iframe src="https://www.youtube.com/embed/' + media_name + '?rel=0&mute=1&modestbranding=1&autohide=1&controls=0" frameborder="0" allow="autoplay" allowfullscreen></iframe>');
     }
     else if(item[0].tagName.toLowerCase() == "video") {
       $('#lightbox-content').html('<div id="lightbox-v-overlay"> <video controls muted> <source src="' + media_href + '"></video></div>');
