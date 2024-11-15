fe4fe3f639398ad3ab06936e7157701b5112af2d
Anevenua <a.anevenua@gmail.com>
2024-11-15 11:47:13 +0100

    mail upgrade

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 ﻿<?php 
 $errors = '';
-$myemail = 'you@yoursite.com';
+$myemail = 'a.anevenua@gmail.com';
 if(empty($_POST['name'])  ||
    empty($_POST['email']) ||
    empty($_POST['subject']) ||
